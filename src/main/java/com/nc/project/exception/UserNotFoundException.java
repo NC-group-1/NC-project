13c1fb72c8b37e6886ad5e202ab3982a7136c05f
evvhenii <65552266+evvhenii@users.noreply.github.com>
2020-11-20 16:17:37 +0200

    Revert "View profile for user"

--- src/main/java/com/nc/project/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nc.project.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
