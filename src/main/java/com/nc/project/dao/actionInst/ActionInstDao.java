c131475beeb80eebd7e8740f40d7dc3fde34856b
savchukanna <52628169+savchukanna@users.noreply.github.com>
2020-12-27 11:15:53 +0200

    fix

--- src/main/java/com/nc/project/dao/actionInst/ActionInstDao.java
+++ src/main/java/com/nc/project/dao/actionInst/ActionInstDao.java
@@ -6,9 +6,13 @@ import com.nc.project.model.ActionInst;
 import java.util.List;
 import java.util.Optional;
 
-public interface ActionInstDao extends GenericDao<ActionInst, Integer>{
+public interface ActionInstDao extends GenericDao<ActionInst, Integer> {
+
     List<ActionInstRunDto> getAllActionInstRunDtosByTestCaseId(int id);
+
     List<ActionInstRunDto> updateAll(List<ActionInstRunDto> actionInstRunDtos);
+
     List<ActionInst> getAllActionInstancesByTestCaseId(Integer testCaseId);
+
     Optional<Integer> getNumberOfActionInstancesByTestCaseId(Integer testCaseId);
 }
